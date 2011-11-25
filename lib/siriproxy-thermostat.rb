ce881412d1e2501e82fcb97f2b64d663dcbe3ec0
Douwe Maan <douwemaan@gmail.com>
2011-11-25 20:14:27 +0100

    Remove unrelated comment in header

--- lib/siriproxy-thermostat.rb
+++ lib/siriproxy-thermostat.rb
@@ -1,14 +1,6 @@
 require 'httparty'
 require 'json'
 
-#######
-# This is a "hello world" style plugin. It simply intercepts the phrase "text siri proxy" and responds
-# with a message about the proxy being up and running (along with a couple other core features). This 
-# is good base code for other plugins.
-# 
-# Remember to add other plugins to the "config.yml" file if you create them!
-######
-
 class SiriProxy::Plugin::Thermostat < SiriProxy::Plugin
   attr_accessor :host
 
